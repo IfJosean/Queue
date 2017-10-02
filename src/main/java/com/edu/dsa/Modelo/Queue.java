06b1b340bd1d3aad0db0e5fd93738083924ab3dd
IfJosean <josean7jkami@gmail.com>
2017-10-02 13:22:19 +0200

    queue and queueimpl

--- src/main/java/com/edu/dsa/Modelo/Queue.java
+++ src/main/java/com/edu/dsa/Modelo/Queue.java
@@ -3,5 +3,11 @@ package com.edu.dsa.Modelo;
 /**
  * Created by Josean on 02/10/2017.
  */
-public interface Queue {
+public interface Queue <E> {
+
+     void push (E e);
+    public E pop();
+    public int size();
+
+
 }
--- src/main/java/com/edu/dsa/Modelo/QueueImpl.java
+++ src/main/java/com/edu/dsa/Modelo/QueueImpl.java
@@ -1,7 +1,36 @@
 package com.edu.dsa.Modelo;
+package com.edu.dsa.s2;
+
+import sun.misc.*;
+import java.util.EmptyQueueException;
 
 /**
  * Created by Josean on 02/10/2017.
  */
-public class QueueImpl {
+public class QueueImpl <E> extends Queue <E>{
+
+    private E[] el;
+    private int i=0;
+
+    public QueueImpl (int q)
+    {
+        this.el = (E[])new Object[q];
+    }
+
+
+    public void push(E e) {
+
+        this.el[this.i++]=e;
+    }
+
+    public E pop() {
+
+        if (i == 0)
+            throw new EmptyQueueException();
+        return null;
+    }
+
+    public int size() {
+        return this.i;
+    }
 }
