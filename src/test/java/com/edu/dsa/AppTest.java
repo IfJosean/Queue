1f5ef5c1e58237e21eb0d682f14f7621e58b6a5a
Mike <crack.mike@gmail.com>
2017-10-02 13:13:23 +0200

    mark contrib

--- src/test/java/com/edu/dsa/AppTest.java
+++ src/test/java/com/edu/dsa/AppTest.java
@@ -7,14 +7,17 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
+
+    public
+
+
     public AppTest( String testName )
     {
         super( testName );
